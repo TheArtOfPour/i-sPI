e0bc48d1b8f320eee188809748e2f7eb60b4df80
TAoP <theartofpour@gmail.com>
2019-11-18 18:30:37 -0800

    Button presses and gitignore

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# import RPi.GPIO as GPIO
 from gpiozero import Button
 from signal import pause
 
 buttonPin = 16
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 button = Button(buttonPin)
-
-
-# def button_press(channel):
-#     print("Button pressed!")
 
 
 def press():
@@ -18,19 +10,11 @@ def press():
 
 
 def release():
-    print("Button pressed!")
+    print("Button released!")
 
 
 button.when_pressed = press
 button.when_released = release
 
-# GPIO.add_event_detect(24, GPIO.RISING, callback=button_press, bouncetime=300)
-
-# condition = True
-# while condition:
-#     time.sleep(300)
-
 pause()
 
-# raw_input("Listening...")
-# GPIO.cleanup()
