9927e5739b2f2cd1203538693da412e453dd5b0f
Justin Revelstoke <theartofpour@gmail.com>
2019-11-18 18:48:23 -0800

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+#!~/i-sPI/.venv/bin python
+
 from gpiozero import Button
 from signal import pause
+from picamera import PiCamera
+from time import sleep
 
 buttonPin = 16
 button = Button(buttonPin)
+camera = PiCamera()
 
 
 def press():
     print("Button pressed!")
+    camera.start_preview()
+    sleep(2)
+    camera.capture('/tmp/picture.jpg')
+    camera.stop_preview()
 
 
 def release():
@@ -17,4 +26,3 @@ button.when_pressed = press
 button.when_released = release
 
 pause()
-
