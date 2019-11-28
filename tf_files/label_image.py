1b9b1f446a019b002c72596d3bcf4f3941295e46
Justin Revelstoke <theartofpour@gmail.com>
2019-11-27 22:56:31 -0800

    fix indentation

--- tf_files/label_image.py
+++ tf_files/label_image.py
@@ -8,13 +8,12 @@ image_path = sys.argv[1]
 # Read in the image_data
 image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 # Loads label file, strips off carriage return
-label_lines = [line.rstrip() for line
-               in tf.gfile.GFile("./retrained_labels.txt")]
+label_lines = [line.rstrip() for line in tf.gfile.GFile("./retrained_labels.txt")]
 # Unpersists graph from file
 with tf.gfile.FastGFile("./retrained_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
-graph_def.ParseFromString(f.read())
-_ = tf.import_graph_def(graph_def, name='')
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
 with tf.Session() as sess:
     # Feed the image_data as input to the graph and get first prediction
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
